654b6426e0cd9093854284ecb119003e86c4645c
jjkyun <jkyunkim01@gmail.com>
2018-07-15 11:06:51 +0900

    Final Commit

--- runningmedian.py
+++ runningmedian.py
@@ -15,10 +15,10 @@ class MaxHeap():
         left = 2 * i     
         right = 2 * i + 1    
         
-        if left < self.size and self.arr[largest] < self.arr[left]:
+        if left <= self.size and self.arr[largest] < self.arr[left]:
             largest = left
     
-        if right < self.size and self.arr[largest] < self.arr[right]:
+        if right <= self.size and self.arr[largest] < self.arr[right]:
             largest = right
     
         if largest != i:
@@ -27,18 +27,20 @@ class MaxHeap():
 
     def push(self, data):       
         self.size += 1
+
         ## 힙의 개수가 2 이상일 때, 
         if self.size > 1:
             self.arr.append(data)
             child = self.size
-            parent = int(self.size/2)
+            parent = int(child/2)
             
             while parent >= 1:
+                parent = int(child/2)
                 if self.arr[child] > self.arr[parent]:
                     ## Swap
                     self.arr[child], self.arr[parent] = self.arr[parent], self.arr[child]
+                    child = parent
                     parent = int(parent/2)
-                    child = int(child/2)
                 else:
                     break
 
@@ -71,11 +73,10 @@ class MinHeap():
         smallest = i
         left = 2 * i     
         right = 2 * i + 1    
-        
-        if left < self.size and self.arr[smallest] > self.arr[left]:
+        if left <= self.size and self.arr[smallest] > self.arr[left]:
             smallest = left
     
-        if right < self.size and self.arr[smallest] > self.arr[right]:
+        if right <= self.size and self.arr[smallest] > self.arr[right]:
             smallest = right
     
         if smallest != i:
@@ -89,14 +90,14 @@ class MinHeap():
         if self.size > 1:
             self.arr.append(data)
             child = self.size
-            parent = int(self.size/2)
+            parent = int(child/2)
             
             while parent >= 1:
                 if self.arr[child] < self.arr[parent]:
                     ## Swap
                     self.arr[child], self.arr[parent] = self.arr[parent], self.arr[child]
+                    child = parent
                     parent = int(parent/2)
-                    child = int(child/2)
                 else:
                     break
 
@@ -109,6 +110,7 @@ class MinHeap():
         self.arr[1], self.arr[self.size] = self.arr[self.size], self.arr[1]
         del self.arr[self.size]
         self.size -= 1
+
         ## heapify
         self.heapify(1)
 
@@ -120,73 +122,42 @@ class MinHeap():
         print(self.arr[1:(self.size+1)])
 
 if __name__ == '__main__':
-    # test_case = int(input())
+    test_case = int(input())
 
-    # for i in range(test_case):
-    #     command = input()
-    #     argument = command.split(' ')
-    #     n, a, b = int(argument[0]), int(argument[1]), int(argument[2])
+    for i in range(test_case):
+        command = input()
+        argument = command.split(' ')
+        n, a, b = int(argument[0]), int(argument[1]), int(argument[2])
 
-    #     result = 0
-    #     previous_value = 1983
-    #     max_heap = MaxHeap(previous_value)
-    #     min_heap = MinHeap()
+        previous_value = 1983
+        max_heap = MaxHeap(previous_value)
+        min_heap = MinHeap()
+        result = 0
 
-    #     '''
-    #     1. max heap의 크기는 min heap의 크기와 같거나 1 더 크다.
-    #     2. max heap의 root 값은 min heap의 root 값보다 작거나 같다.
-    #     '''        
-    #     for i in range(0, n):
-    #         next_value = (previous_value * a + b) % 20090711
+        '''
+        1. max heap의 크기는 min heap의 크기와 같거나 1 더 크다.
+        2. max heap의 root 값은 min heap의 root 값보다 작거나 같다.
+        '''        
+        for i in range(0, n):
+            result = (result + max_heap.top())
+            next_value = (previous_value * a + b) % 20090711
 
-    #         ## 힙의 개수가 같으면 max heap에 넣는다
-    #         if max_heap.size == min_heap.size:
-    #             max_heap.push(next_value)
-    #         ## 힙의 개수가 다르면 min heap에 넣는다
-    #         else:
-    #             min_heap.push(next_value)
+            ## 힙의 개수가 같으면 max heap에 넣는다
+            if max_heap.size == min_heap.size:
+                max_heap.push(next_value)
+            ## 힙의 개수가 다르면 min heap에 넣는다
+            else:
+                min_heap.push(next_value)
 
-    #         max_heap.print()
-    #         min_heap.print()
-    #         input()
-    #         ## 힙이 둘다 비어있지 않고, max heap의 최대 값이 min heap의 최소 값보다 크다면 root 값끼리 바꾼다
-    #         if min_heap.size != 0 and max_heap.size != 0 and min_heap.top() < max_heap.top():
-    #             a = max_heap.top()
-    #             b = min_heap.top()
-    #             max_heap.pop()
-    #             min_heap.pop()
-    #             print('pop이 된 후')
-    #             max_heap.print()
-    #             min_heap.print()
-    #             input()
+            ## 힙이 둘다 비어있지 않고, max heap의 최대 값이 min heap의 최소 값보다 크다면 root 값끼리 바꾼다
+            if min_heap.size != 0 and max_heap.size != 0 and min_heap.top() < max_heap.top():
+                m = max_heap.top()
+                n = min_heap.top()
+                max_heap.pop()
+                min_heap.pop()
+                max_heap.push(n)
+                min_heap.push(m)
                 
-    #             max_heap.push(b)
-    #             min_heap.push(a)
+            previous_value = next_value
 
-    #             print('바뀐 후')
-    #             max_heap.print()
-    #             min_heap.print()
-    #             input()
-
-    #         print('중앙값')
-    #         print(max_heap.top())
-    #         input()
-    #         result = (result + max_heap.top()) % 20090711
-    #         previous_value = next_value
-
-    #     print(result)
-    heap = MaxHeap(3)
-    heap.push(1)
-    heap.push(4)
-    heap.push(13)
-    heap.push(5)
-    heap.print()
-    heap.pop()
-    heap.pop()
-    heap.print()
-
-    heap.push(12)
-    heap.push(7)
-    heap.print()
-
-
+        print(result % 20090711)
